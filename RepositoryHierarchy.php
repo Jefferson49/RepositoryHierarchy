f7bef9a545ea8784819e6688ff1552723386662e
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2022-09-30 07:08:53 +0200

    Fixed typo in 'Repository Hierarchy of: %s'

--- RepositoryHierarchy.php
+++ RepositoryHierarchy.php
@@ -648,7 +648,7 @@ class RepositoryHierarchy   extends     AbstractModule
         if ($repository === null) {
             return I18N::translate('Repository Hierarchy');
         } else {
-            return I18N::translate('Repository Hierachy of: %s', $repository->fullName());
+            return I18N::translate('Repository Hierarchy of: %s', $repository->fullName());
         }
     }
 
