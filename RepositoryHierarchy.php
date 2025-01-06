9a181c8e161a9c66cc266b13032a42af8ca1d7ab
Jefferson49 <81484983+Jefferson49@users.noreply.github.com>
2025-01-06 15:59:50 +0100

    Increased version to 1.5.1

--- RepositoryHierarchy.php
+++ RepositoryHierarchy.php
@@ -105,7 +105,7 @@ class RepositoryHierarchy extends AbstractModule implements
     use ModuleDataFixTrait;
 
     //Custom module version
-    public const CUSTOM_VERSION = '1.5.0';
+    public const CUSTOM_VERSION = '1.5.1';
 
     //Routes, attributes
     protected const MODULE_NAME_IN_ROUTE = 'repositoryhierarchy';
